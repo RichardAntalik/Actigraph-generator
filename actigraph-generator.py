6c6b322057b56e6b47da4671394b69b18befd48e
Richard Antalík <richardantalik@gmail.com>
2024-08-08 06:44:06 +0200

    It's better this way, trust me

--- actigraph-generator.py
+++ actigraph-generator.py
@@ -3,19 +3,12 @@ import datetime
 import math
 from PIL import Image as im, ImageDraw, ImageFont
 
-#plan is to generate data for 763 days.
-#That is 763px wide/tall image. make it in 2x scale...
-# another aspect of the image should be 24 hours. I want to have plot lines, so let's make this at 8x scale
-# so the image is 1526 x 192
-
 # Globals
-log_file = "/home/me/sleep_log_2020.txt"
-#log_file = "/home/me/sleep_log.txt"
+log_file = "/home/me/sleep_log.txt"
 entry_time_fmt = "%d.%m.%Y %H:%M:%S"
 time_scale = 10
-day_scale = 4   
-act_col = 85
-day_pixel_offset = 0
+day_scale = 4
+activity_color = 85
 padding_date = 90
 padding_time = 60
 
@@ -30,11 +23,9 @@ def parse(log):
         entry = entry.replace("bedtime: ", "bedtime|")
         entry = entry.replace("wakeup: ", "wakeup|")
         entry = entry.split('|')
-        #date = entry[-1].split(' ')
-        #entry.pop()
-        #entry.extend(date)
-        entries.append(entry)
-
+        date = datetime.datetime.strptime(entry[1], entry_time_fmt)
+        entries.append([entry[0], date])
+        
     # Manual data validation - case for unexpected mid-day entries.
     #for i in range(2, len(entries)):
     #    if entries[i][1] == entries[i-2][1]:
@@ -46,20 +37,22 @@ def parse(log):
 def make_img(entries):
     bg_col = 255
     width = 24 * time_scale + padding_date
-    num_lines = int(len(entries)/2)
-    height = num_lines * day_scale + num_lines * day_pixel_offset + padding_time
+    start_date = entries[0][1]
+    end_date = entries[len(entries)-1][1]
+    num_lines = (end_date - start_date).days + 1
+    height = num_lines * day_scale + padding_time
     return np.full((width, height), bg_col, np.uint8)
 
 
 def fill_range(day_offset, hour_start, hour_end, image, col):
-        day_pix_start = day_offset * day_scale + padding_time
-        day_range = slice(day_pix_start, day_pix_start + day_scale)
+    day_pix_start = day_offset * day_scale + padding_time
+    day_range = slice(day_pix_start, day_pix_start + day_scale)
 
-        time_pix_start = int(hour_start * time_scale) + padding_date
-        time_pix_end = int(hour_end * time_scale) + padding_date
-        time_range = slice(time_pix_start, time_pix_end)
-        x = np.flip(image.T[day_range])
-        x.T[time_range] = col
+    time_pix_start = int(hour_start * time_scale) + padding_date
+    time_pix_end = int(hour_end * time_scale) + padding_date
+    time_range = slice(time_pix_start, time_pix_end)
+    x = np.flip(image.T[day_range])
+    x.T[time_range] = col
 
 
 def draw_grid(image, entries):
@@ -71,7 +64,7 @@ def draw_grid(image, entries):
     font_size = 10
     font = ImageFont.truetype("FreeMono", size=font_size)
     
-    day = datetime.datetime.strptime(entries[0][1], entry_time_fmt).date()
+    day = entries[0][1].date()
     first = False
     for x in range(padding_time - 1, width, day_scale):
         draw.line([x, 0, x, height - padding_date], 0)
@@ -101,19 +94,19 @@ def draw_grid(image, entries):
 
 def make_actigraph(entries):
     array = make_img(entries)
-    start_day = datetime.datetime.strptime(entries[0][1], entry_time_fmt).date()
+    start_day = entries[0][1].date()
 
     # Mark sunday columns by different color
-    for i in range(0, len(entries) - 1, 2):
-        entry_start = datetime.datetime.strptime(entries[i][1], entry_time_fmt)
+    for i in range(0, len(entries), 2):
+        entry_start = entries[i][1]
         if entry_start.date().strftime("%a") == "Sun":
             day_offset = (entry_start.date() - start_day).days
             fill_range(day_offset, 0, 24, array, 200)
 
 
     for i in range(0, len(entries) - 1, 2):
-        entry_start = datetime.datetime.strptime(entries[i][1], entry_time_fmt)
-        entry_end = datetime.datetime.strptime(entries[i+1][1], entry_time_fmt)
+        entry_start = entries[i][1]
+        entry_end = entries[i+1][1]
 
         day_offset = (entry_start.date() - start_day).days
         start_time = entry_start.time()
@@ -122,12 +115,11 @@ def make_actigraph(entries):
         end_hour = end_time.hour + end_time.minute / 60
 
         if entry_start.day != entry_end.day:
-            fill_range(day_offset, start_hour, 24, array, act_col)
-            fill_range(day_offset+1, 0, end_hour, array, act_col)
-            print(entry_end, end_hour)
+            fill_range(day_offset, start_hour, 24, array, activity_color)
+            fill_range(day_offset+1, 0, end_hour, array, activity_color)
 
         else:
-            fill_range(day_offset, start_hour, end_hour, array, act_col)
+            fill_range(day_offset, start_hour, end_hour, array, activity_color)
 
     image = im.fromarray(array) 
     draw_grid(image, entries)
@@ -136,12 +128,14 @@ def make_actigraph(entries):
 ################# 
 
 entries = parse(open(log_file, "r").read())
-entries_per_graph = math.ceil(len(entries) / 3) # Note, that days = entries_per_graph / 2
+entries.append(["bedtime: ", datetime.datetime.now()]) # Needed to show current day.
+entries_per_graph = math.ceil(len(entries)) # Note, that days = entries_per_graph / 2.
 pages = math.ceil(len(entries) / entries_per_graph)
 
 for page in range(0, pages):
     start_index = page * entries_per_graph
     entries_page = entries[start_index: start_index + entries_per_graph]
     image = make_actigraph(entries_page)
+
     image.show()
-    #image.save('/home/me/Desktop/act.png')
+    image.save('cur.png')
